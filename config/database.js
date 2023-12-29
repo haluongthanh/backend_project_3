d4539aebc1a9e9d900923c1aa7ed46d2f4ec372d
Ha Luong Thanh <haluongthanh2003@gmail.com>
2023-12-29 14:32:00 +0700

    Your commit message

--- config/database.js
+++ config/database.js
@@ -1,9 +1,18 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URI)
-    .then((data)=>{
-        console.log(`Mongodb connected with server : ${data.connection.host}`);
-    })
-}
-module.exports=connectDatabase;
+const connectDatabase = () => {
+    mongoose.set('strictQuery', false); // Add this line to address the deprecation warning
+
+    mongoose.connect(process.env.DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        .then((data) => {
+            console.log(`Mongodb connected with server : ${data.connection.host}`);
+        })
+        .catch((error) => {
+            console.error('Mongodb connection error:', error);
+        });
+};
+
+module.exports = connectDatabase;
